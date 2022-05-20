ae4d62bef06816afebd3eabdc758338df98309ee
muhammad-azriel-cp <102741897+muhammad-azriel-cp@users.noreply.github.com>
2022-05-20 17:31:11 +0700

    Add files via upload

--- lib/Fakultas.dart
+++ lib/Fakultas.dart
@@ -9,19 +9,75 @@ class Fakultas extends StatelessWidget {
     return Center(
       child: ListView(padding: EdgeInsets.all(20), shrinkWrap: true, children: [
         InkWell(
-          child: Container(
-            decoration: BoxDecoration(border: Border.all()),
-            padding: EdgeInsets.all(14),
-            child: Text("baris 1"),
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Container(
+              decoration: BoxDecoration(border: Border.all()),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    width: 300,
+                    child: Column(children: const [
+                      Text(
+                        "FPMIPA",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 25),
+                      ),
+                      Text(
+                          "Fakultas Pendidikan Matematika dan Ilmu Pengetahuan Alam")
+                    ]),
+                  ),
+                  Column(children: [
+                    Center(
+                        child: Container(
+                            padding: const EdgeInsets.all(10),
+                            child: Image.network(
+                              'asset/download.jpg',
+                              width: 120,
+                            )))
+                  ]),
+                ],
+              ),
+            ),
           ),
           onTap: () {
-            //gunakan navigator untuk panggil RincianFakultas
+            Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+              return RincianFakultas();
+            }));
           },
         ),
-        Container(
+        Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Container(
             decoration: BoxDecoration(border: Border.all()),
-            padding: EdgeInsets.all(14),
-            child: Text("baris kedua")),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  width: 300,
+                  child: Column(children: const [
+                    Text(
+                      "FPIPS",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                    ),
+                    Text("Fakultas Pendidikan Ilmu Pengetahuan Sosial")
+                  ]),
+                ),
+                Column(children: [
+                  Center(
+                      child: Container(
+                          padding: const EdgeInsets.all(10),
+                          child: Image.network(
+                            'asset/download (1).jpg',
+                            width: 120,
+                          )))
+                ]),
+              ],
+            ),
+          ),
+        ),
       ]),
     );
   }
--- lib/RincianFakultas.dart
+++ lib/RincianFakultas.dart
@@ -9,7 +9,30 @@ class RincianFakultas extends StatelessWidget {
         title: const Text('Rincian Fakultas'),
       ),
       body: Center(
-        child: Text("rincian fakultas"),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Column(children: [
+                Image.network('asset/download.jpg'),
+                const Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Text(
+                      'Fakultas Pendidikan Matematika dan Ilmu Pengetahuan Alam',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 25)),
+                ),
+                Text(
+                    'Fakultas Pendidikan Matematika dan Ilmu Pengetahuan Alam (FPMIPA) merupakan unsur pelaksana akademik',
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 12)),
+                Text(
+                    'yang bertugas mengkoordinir pelaksanaan kegiatan akademik di bidang pendidikan MIPA dan MIPA.',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12))
+              ]),
+            ),
+          ],
+        ),
       ),
     );
   }
